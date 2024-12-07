4be59628ccfdb40e1ba7c57f346aee7e6a131234
SY-Ang <130272690+SY-Ang@users.noreply.github.com>
2024-12-07 18:15:55 +0800

    task 1-4_r2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -88,7 +88,7 @@ values (
 (select price from "CREDIT_PACKAGE" where name='14 堂組合包方案')
 )
     ); 
-原本沒考慮到要用子查詢加入credit_amount跟price，參考了直播課內容才加入
+-- 原本沒考慮到要用子查詢加入credit_amount跟price，參考了直播課內容才加入
 
 
 -- ████████  █████   █    ████   
