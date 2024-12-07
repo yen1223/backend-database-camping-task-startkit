12df2c5ab8adbbd6767457f5054e5c9b52feaede
SY-Ang <130272690+SY-Ang@users.noreply.github.com>
2024-12-07 13:20:44 +0800

    task 1-2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -14,14 +14,34 @@
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
+--insert into "USER" (name,email,role)
+--VALUES
+--('李燕容','lee2000@hexschooltest.io','USER'),
+--('王小明','wXlTq@hexschooltest.io','USER'),
+--('肌肉棒子','muscle@hexschooltest.io','USER'),
+--('好野人','richman@hexschooltest.io','USER'),
+--('Q太郎','starplatinum@hexschooltest.io','USER'),
+--('透明人','opacity0@hexschooltest.io','USER');
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
+--update "USER"
+--set role='COACH'
+--where role='USER' and "USER".email in ('lee2000@hexschooltest.io','muscle@hexschooltest.io','starplatinum@hexschooltest.io');
+
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
+
+--delete from "USER" 
+--where email='opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 
+--select COUNT(*) from "USER" ;
+
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
+
+--select * from "USER" 
+--limit 3;
 
 
 --  ████████  █████   █    ████  
@@ -36,10 +56,46 @@
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
 
+--insert into "CREDIT_PACKAGE" (name, price, credit_amount)
+--values
+--('7 堂組合包方案',1400,7),
+--('14 堂組合包方案',2520,14),
+--('21 堂組合包方案',4800,21);
+
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
+
+--select id 
+--from "USER" 
+--where email='wXlTq@hexschooltest.io';
+
+--select id 
+--from "CREDIT_PACKAGE"  
+--where "name"='14 堂組合包方案';
+
+--insert into "CREDIT_PURCHASE" (user_id,credit_package_id,purchased_credits,price_paid)
+--values (
+--(
+--(select id from "USER" where email='wXlTq@hexschooltest.io'),
+--(select id from "CREDIT_PACKAGE" where name='14 堂組合包方案'),
+--(select credit_amount from "CREDIT_PACKAGE" where name='14 堂組合包方案'),
+--(select price from "CREDIT_PACKAGE" where name='14 堂組合包方案')
+--),
+--(
+--(select id from "USER" where email='wXlTq@hexschooltest.io'),
+--(select id from "CREDIT_PACKAGE" where name='21 堂組合包方案'),
+--(select credit_amount from "CREDIT_PACKAGE" where name='21 堂組合包方案'),
+--(select price from "CREDIT_PACKAGE" where name='21 堂組合包方案')
+--),
+--(
+--(select id from "USER" where email='richman@hexschooltest.io'),
+--(select id from "CREDIT_PACKAGE" where name='14 堂組合包方案'),
+--(select credit_amount from "CREDIT_PACKAGE" where name='14 堂組合包方案'),
+--(select price from "CREDIT_PACKAGE" where name='14 堂組合包方案')
+--)
+--); 原本沒考慮到要用子查詢加入credit_amount跟price，參考了直播課內容才加入
 
 
 -- ████████  █████   █    ████   
